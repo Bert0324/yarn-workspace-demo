ec8b03c04493ea7c7382fcf6ad8b77378c105de0
yuchen.huang <hyc231921@alibaba-inc.com>
2021-08-26 20:45:13 +0800

    init

---
+++ mockNpmPackage/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  packagePath: () => console.log(__dirname)
+}
---
+++ packages/p1/index.js
@@ -0,0 +1,5 @@
+const a = require('@yarn-workspace/p2');
+const b = require('@yarn-workspace-demo/published');
+
+a.foo();
+b.packagePath();
